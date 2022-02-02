44313f18f4f33f63142b17e81be7f5fe3866c106
karami4yasser <83478271+karami4yasser@users.noreply.github.com>
2022-02-02 10:38:55 +0100

    Update Nav.js

--- components/Nav.js
+++ components/Nav.js
@@ -9,7 +9,7 @@ function Nav() {
         className="flex px-10 
       sm:px-20 text-2xl whitespace-nowrap 
       space-x-10 sm:space-x-20 overflow-x-scroll 
-      forsmall:scrollbar-hide"
+      scrollbar-hide"
       >
         {Object.entries(requests).map(([key, { title, url }]) => (
           <h2
